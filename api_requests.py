8736bfff4fb88820efa0ee3f707d15c682173db7
jrooney05 <sgjroone@liverpool.ac.uk>
2025-04-14 17:32:22 +0100

    Update api_requests.py

--- api_requests.py
+++ api_requests.py
@@ -1,21 +1,19 @@
 import requests
-import sys
 
-waypoints = sys.argv[1] # where waypoints has some variables long and lat for the longitude and latitude of the node respectively 
-coordinates = ""
-# string formatting for input into mapbox api
-for node in waypoints:
-    coordinates = coordinates + waypoints["long"] + "," + waypoints["lat"] + ";"
-coordinates = coordinates[:-1] # remove last ; from coordinates string
+def get_order(waypoints):
+    coordinates = ""
+    for node in waypoints:
+        coordinates = coordinates + node["long"] + "," + node["lat"] + ";"
+    coordinates = coordinates[:-1]
 
-access_token = "" # api token
-url = f"https://api.mapbox.com/optimized-trips/v1/mapbox/driving/{coordinates}?geometries=geojson&access_token={access_token}"
+    access_token = "pk.eyJ1IjoianJvb25leTA1IiwiYSI6ImNtOHE1ODE4YjA5YTMyaXNocGE4OTl6eGwifQ.vS0CZaN1jviMfPYsj4UWNQ"
+    url = f"https://api.mapbox.com/optimized-trips/v1/mapbox/driving/{coordinates}?geometries=geojson&access_token={access_token}"
 
-response = requests.get(url)
-data = response.json()
+    response = requests.get(url)
+    data = response.json()
 
-# Get the optimized route geometry and order
-route = data["trips"][0]["geometry"]
-waypoint_order = [wp["waypoint_index"] for wp in data["waypoints"]] # retrieves node id for each node/waypoint
+    # Get the optimized route geometry and order
+    route = data["trips"][0]["geometry"]
+    waypoint_order = [wp["waypoint_index"] for wp in data["waypoints"]] # retrieves node id for each node/waypoint
 
-print("Optimized order of stops:", waypoint_order)
+    print("Optimized order of stops:", waypoint_order)
